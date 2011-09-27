79e48561431084fa44e1d9cc250e122e2bb5bf50
Andrey Grygoryev <undeadgrandse@gmail.com>
2011-09-27 21:47:24 +0300

    Base http response and request objects

--- lighty/wsgi/handler.py
+++ lighty/wsgi/handler.py
@@ -1,9 +1,56 @@
-import sys, traceback
-
 from functools import partial
 
 from lighty.exceptions import ApplicationException, NotFoundException
 from urls import resolve, url
+
+try:
+    import cStringIO as StringIO
+except:
+#    try:
+#        import StringIO
+#    except:
+#        import io as StringIO
+    pass
+
+class Request(object):
+    '''WSGI request wrapper
+    '''
+    __slots__ = ('method', 'headers', 'cookies', 'path', 'params', 'get')
+
+    def __init__(self, environ):
+        self.protocol   = environ['SERVER_PROTOCOL']
+        self.method     = environ['REQUEST_METHOD']
+        self.path       = environ['PATH_INFO']
+        self.query      = environ['QUERY_STRING']
+        #self.accept_lan = environ['HTTP_ACCEPT_LANGUAGE']
+        #self.accept_chr = environ['HTTP_ACCEPT_CHARSET']
+        #self.accept     = environ['HTTP_ACCEPT']
+        #self.cookie     = environ['HTTP_COOKIE']
+        #self.params = {}
+        # 'CONTENT_LENGTH': '', 'HTTP_CONNECTION': 'keep-alive', 'CONTENT_TYPE': 'text/plain', 'SSH_AUTH_SOCK': '/tmp/keyring-qn1yMR/ssh', 'wsgi.multithread': True, 'GDMSESSION': 'ubuntu-2d', 'REMOTE_ADDR': '127.0.0.1', 'HTTP_USER_AGENT': 'Mozilla/5.0 (X11; Linux x86_64; rv:7.0) Gecko/20100101 Firefox/7.0', 'HTTP_HOST': '127.0.0.1:8000'
+        # 'REMOTE_HOST': 'localhost', 'HTTP_ACCEPT_ENCODING': 'gzip, deflate',
+
+    def get(self, name, default=None):
+        return name in self.params and self.params[name] or default
+
+
+class Response(object):
+    '''Class used to put data into response
+    '''
+    __slots__ = ('buffer', 'write', 'headers', 'cookies', 'status', 'start')
+
+    def __init__(self, data, content_type='text/html'):
+        self.status     = '200 OK'
+        self.headers    = [('Content-type', content_type)]
+        self.buffer     = StringIO.StringIO()
+        self.buffer.write(data)
+
+    def start(self, start_response_func):
+        result_data = self.buffer.getvalue()
+        self.buffer.close()
+        self.headers.append(('Content-Length', str(len(result_data))))
+        start_response_func(self.status, self.headers)
+        return result_data
 
 
 def test():
@@ -26,27 +73,16 @@ class WSGIApplication(object):
     def handler(self, environ, start_response):
         '''Basic function for responsing
         '''
-        headers = [('Content-type', 'text/plain')]
-
-        try:
-            # Create new request and process middleware
-            view    = self.resolve(environ['PATH_INFO'],
-                                   environ['REQUEST_METHOD'])
-            if issubclass(view.__class__, Exception): raise view
-            msg     = view()
-            status  = '200 OK'
-        except NotFoundException as exc:
-            traceback.print_exc(file=sys.stdout)
-            status = '404 Page was now found'
-            msg = str(exc)
-        except ApplicationException as appexc:
-            traceback.print_exc(file=sys.stdout)
-            status = '500 Internal Server Error'
-            msg = appexc and str(appexc)
-        except Exception as exc:
-            traceback.print_exc(file=sys.stdout)
-            status = '500 Internal Server Error'
-            msg = str(exc)
-        finally:
-            start_response(status, headers)
-            return status + '\n' + msg
+        # Create new request and process middleware
+        view    = self.resolve(environ['PATH_INFO'],
+                               environ['REQUEST_METHOD'])
+        response = Response(issubclass(view.__class__, Exception) and str(view)
+                            or view())
+        response.status = ((view.__class__ == NotFoundException and
+                                '404 Page was not found') or
+                           (view.__class__ == ApplicationException and
+                                '500 Internal Server Error') or
+                           (view.__class__ == ApplicationException and
+                                '500 Internal Server Error') or
+                           '200 OK')
+        return response.start(start_response)
