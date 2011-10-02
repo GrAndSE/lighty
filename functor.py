59cc5d9abaf505ee5e4eed32052fd571889218d6
Andrey Grygoryev <undeadgrandse@gmail.com>
2011-10-02 11:02:16 +0300

    Make functors working

--- functor.py
+++ functor.py
@@ -1,74 +1,100 @@
-class Field(object):
+class BaseField(object):
     '''Base field class
     '''
-
     def __init__(self):
-        super(Field, self).__init__()
+        super(BaseField, self).__init__()
 
-    # Logical operators
-
-    def __and__(self, other):
-        print 'And'
-
-    def __or__(self, other):
-        print 'Or'
-
-    def __xor__(self, other):
-        print 'Xor'
-
-    # Numeric
-
-    def __add__(self, other):
-        print '+'
-
-    def __sub__(self, other):
-        print '-'
-
-    def __mul__(self, other):
-        print '*'
-
-    def __div__(self, other):
-        print '/'
-
-    def __mod__(self, other):
-        print '%'
-
-    def __pow__(self, other):
-        print '**'
-
-    # Comparision
+    def create_functor(self, operator, operand):
+        return FieldFunctor(self, operator, operand)
 
     def __lt__(self, other):
-        print '<'
+        return self.create_functor('<', other)
 
     def __gt__(self, other):
-        print '>'
+        return self.create_functor('>', other)
 
     def __le__(self, other):
-        print '<='
+        return self.create_functor('<=', other)
 
     def __ge__(self, other):
-        print '>='
+        return self.create_functor('>=', other)
 
     def __eq__(self, other):
-        print '=='
+        return self.create_functor('==', other)
 
     def __ne__(self, other):
-        print '!='
+        return self.create_functor('!=', other)
 
-    def __nonzero__(self, other):
-        print 'bool'
 
-    # Sequences
+class NumericField(BaseField):
+    '''Base class for any numerical fields. It can be used to create:
 
+        integers
+        floats
+        decimals
+    '''
+    def __add__(self, other):
+        return self.create_functor('+', other)
+
+    def __sub__(self, other):
+        return self.create_functor('-', other)
+
+    def __mul__(self, other):
+        return self.create_functor('*', other)
+
+    def __div__(self, other):
+        return self.create_functor('/', other)
+
+    def __mod__(self, other):
+        return self.create_functor('%', other)
+
+    def __pow__(self, other):
+        return self.create_functor('**', other)
+
+
+class SequenceField(BaseField):
+    '''Class used for sequences fields creations. It can be used to create:
+
+        strings
+        arrays
+        dictionaries
+    '''
     def __len__(self):
-        print 'len'
+        return 'len'
 
     def __getitem__(self, key):
-        print '[%s]' % key
+        return self.create_functor('[]', key)
 
     def __contains__(self, item):
-        print item, 'in'
+        return self.create_functor('in', item)
 
     def __getslice__(self, i, j):
-        print '[%d:%s]' % (i, j)
+        return self.create_functor('[%d:%s]', (i, j))
+
+
+class FieldFunctor(BaseField):
+    '''Class used to keep operations history
+    '''
+    __slots__ = ('parent', 'operator', 'operand')
+
+    def __init__(self, parent, operator, operand):
+        super(FieldFunctor, self).__init__()
+        self.parent     = parent
+        self.operator   = operator
+        self.operand    = operand
+
+    def __and__(self, other):
+        return self.create_functor('and', other)
+
+    def __or__(self, other):
+        return self.create_functor('or', other)
+
+    def __xor__(self, other):
+        return self.create_functor('xor', other)
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __str__(self):
+        return '(%s %s %s)' % (str(self.parent),
+                               self.operator, str(self.operand))
