677a524f049ee52997d2aad319df0d4e41c836cf
Andrey Grygoryev <undeadgrandse@gmail.com>
2012-04-07 22:29:09 +0300

    Add missing decorators file

---
+++ lighty/wsgi/decorators.py
@@ -0,0 +1,24 @@
+'''
+'''
+import functools
+import operator
+from .. import monads
+
+
+def view(func, **constraints):
+    '''Functions that decorates a view. This function can also checks the
+    argument values
+    '''
+    func.is_view = True
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        try:
+            if not functools.reduce(operator.__and__,
+                                    [constraints[arg](kwargs[arg])
+                                     for arg in constraints]):
+                return monads.NoneMonad(ValueError(
+                                                'Wrong view argument value'))
+            return monads.ValueMonad(func(*args, **kwargs))
+        except Exception as e:
+            return monads.NoneMonad(e)
+    return wrapper
