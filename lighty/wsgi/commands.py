6c18dbc4bd560e1605782121816fccd692737548
Andrey Grygoryev <undeadgrandse@gmail.com>
2012-04-09 16:12:46 +0300

    Small wsgi server runngin script refactoring

--- lighty/wsgi/commands.py
+++ lighty/wsgi/commands.py
@@ -1,12 +1,13 @@
+def make_application(settings):
+    from . import WSGIApplication
+    return WSGIApplication(settings)
 
 
 def run_server(settings):
     '''Run application using wsgiref test server
     '''
     from wsgiref.simple_server import make_server
-    from . import WSGIApplication
-    application = WSGIApplication(settings)
-    httpd = make_server('', 8000, application)
+    httpd = make_server('', 8000, make_application(settings))
     print("Serving on port 8000...")
     httpd.serve_forever()
 
@@ -15,9 +16,8 @@ def run_tornado(settings):
     '''Run application using Tornade Web framework WSGI server
     '''
     from tornado import ioloop, httpserver, wsgi
-    from . import WSGIApplication
-    application = WSGIApplication(settings)
-    container = wsgi.WSGIContainer(application)
+    container = wsgi.WSGIContainer(make_application(settings))
     http_server = httpserver.HTTPServer(container)
     http_server.listen(8000)
+    print("Serving on port 8000...")
     ioloop.IOLoop.instance().start()
