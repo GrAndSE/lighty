ca6795f7aef809bcd564059005c9bae714888605
Andrey Grygoryev <undeadgrandse@gmail.com>
2012-04-13 16:15:00 +0300

    Fix small issue in static files handler

--- lighty/wsgi/static.py
+++ lighty/wsgi/static.py
@@ -6,7 +6,7 @@ from . import http, urls
 mimetypes.init()
 
 
-def static_view(request, path):
+def static_view(request, path=''):
     '''Serve static files
     '''
     settings = request.app.settings
@@ -14,12 +14,14 @@ def static_view(request, path):
     static_url = (settings.static_url if settings.static_url.endswith('/')
                   else settings.static_url + '/')
     static_root = os.path.realpath(settings.static_root)
+    if not url.startswith(static_url):
+        return http.Response('%s was not found' % url, 404)
     file_path = os.path.join(static_root, url.replace(static_url, ''))
     with open(file_path, 'rb') as file:
         result = "".join(file.readlines())
         headers = None
-        if '.' in path:
-            _, ext = os.path.splitext(path)
+        if '.' in file_path:
+            _, ext = os.path.splitext(file_path)
             if ext in mimetypes.types_map:
                 headers = [('Content-Type', mimetypes.types_map[ext])]
         if not headers:
