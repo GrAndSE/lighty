8be4e3b1c633ee9c74b6ff1107a416620de1b1bb
Andrey Grygoryev <undeadgrandse@gmail.com>
2012-04-19 17:13:13 +0300

    Make basic error handling traceback

--- lighty/wsgi/decorators.py
+++ lighty/wsgi/decorators.py
@@ -2,6 +2,8 @@
 '''
 import functools
 import operator
+import sys
+import traceback
 
 from . import http
 
@@ -24,6 +26,22 @@ def view(func, **constraints):
                 if not isinstance(response, http.Response):
                     response = http.Response(response)
         except Exception as e:
-            response = http.Response(e, 500)
+            try:
+                if args[0].app.settings.debug:
+                    template = args[0].app.get_template('debug.html')
+                    exc_type, exc_value, exc_traceback = sys.exc_info()
+                    tb_list = traceback.extract_tb(exc_traceback)
+                    result = template({
+                        'error_type': exc_value.__class__.__name__,
+                        'error_message': str(exc_value),
+                        'traceback': [{'line': line, 'file': file, 
+                                       'func': name, 'code': code}
+                                      for file, line, name, code in tb_list]
+                    })
+                else:
+                    result = e
+            except:
+                result = e
+            response = http.Response(result, 500)
         return response
     return wrapper
